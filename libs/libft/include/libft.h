0602c6b1c782f5c5f89b124b718b4fda25c3ab13
renato <felicio95.jr@gmail.com>
2022-07-14 08:54:54 -0300

    feat: Adds ft_strcmp to libft project

--- libs/libft/include/libft.h
+++ libs/libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: rfelicio <rfelicio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 16:13:03 by rfelicio          #+#    #+#             */
-/*   Updated: 2021/08/22 17:51:51 by rfelicio         ###   ########.fr       */
+/*   Updated: 2022/07/13 21:08:06 by rfelicio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(char *s1, char *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);

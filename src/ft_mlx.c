3a3a5f2da1e947b959d7369aac0d8c7b119a8102
renato <felicio95.jr@gmail.com>
2022-07-25 10:09:16 -0300

    feat: Mlx initial config: init, window, image, data_addr and loop

--- src/ft_mlx.c
+++ src/ft_mlx.c
@@ -6,7 +6,7 @@
 /*   By: rfelicio <rfelicio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 09:40:59 by rfelicio          #+#    #+#             */
-/*   Updated: 2022/07/14 10:41:05 by rfelicio         ###   ########.fr       */
+/*   Updated: 2022/07/25 10:08:29 by rfelicio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,22 @@
 
 void	ft_mlx_init(t_fractol *fractol)
 {
-	printf("fractol name: %s\n", fractol->name);
 	fractol->mlx = mlx_init();
 	if (!fractol->mlx)
 		ft_puterror(e_mlx_bad_init);
+	fractol->mlx_win = mlx_new_window(fractol->mlx, 700, 700, fractol->name);
+	if (!fractol->mlx_win)
+		ft_puterror(e_mlx_bad_init);
+	fractol->img.img = mlx_new_image(fractol->mlx, 700, 700);
+	if (!fractol->img.img)
+		ft_puterror(e_mlx_bad_init);
+	fractol->img.addr = mlx_get_data_addr(
+			fractol->img.img,
+			&(fractol->img.bits_per_pixel),
+			&(fractol->img.line_lenght),
+			&(fractol->img.endian)
+			);
+	if (!fractol->img.addr)
+		ft_puterror(e_mlx_bad_init);
+	mlx_loop(fractol->mlx);
 }
